95912076a0605fc9e6b72855abcef7601618d9ce
AhmedRefat10 <69695935+AhmedRefat10@users.noreply.github.com>
2022-01-01 22:04:59 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,50 +1,20 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import {Route , Routes} from 'react-router-dom';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+import NewMeetupPage from './pages/NewMeetup';
+import AllMeetupsPage from './pages/AllMeetups';
+import FavouritesPage from './pages/Favourites';
 
-  handleClick = api => e => {
-    e.preventDefault()
+import Layout from "./components/layout/Layout";
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path='/' element={<AllMeetupsPage/>} exact />
+        <Route path='/favorites' element={<FavouritesPage/>} />
+        <Route path='/new-meetup' element={<NewMeetupPage/>} />
+      </Routes>
+    </Layout>
     )
-  }
 }
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { FavoritesContextProvider } from './store/favorites-context'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <FavoritesContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FavoritesContextProvider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
